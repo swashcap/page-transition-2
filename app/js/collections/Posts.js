5a2f06fb5e9a9bd635e435e7349f31d384e0149a
Cory Reed <swashcap@gmail.com>
2015-05-16 23:32:36 -0700

    Implement posts ordering by date.

--- app/js/collections/Posts.js
+++ app/js/collections/Posts.js
@@ -4,6 +4,16 @@
     var App = window.App || {};
 
     App.Collections.Posts = Backbone.Collection.extend({
-        model: App.Models.Post
+        model: App.Models.Post,
+        comparator: function (a, b) {
+            var aDate = a.get('date');
+            var bDate = b.get('date');
+
+            if (aDate instanceof Date && bDate instanceof Date) {
+                return aDate.getTime() < bDate.getTime() ? 1 : -1;
+            } else {
+                return 0;
+            }
+        }
     });
 })(window, Backbone);
